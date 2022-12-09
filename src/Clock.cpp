7835931f2ac4f9d1375a5da03b087aeae29a0811
Leon MARIOTTO <lmariott@e1r11p5.clusters.42paris.fr>
2022-12-09 05:42:48 +0100

    add clock/timer management : the timer interrupt is called but the gb-rom-test still fail i dont know why, everything looks ok, this is annoying

--- src/Clock.cpp
+++ src/Clock.cpp
@@ -6,63 +6,77 @@
 /*   By: nallani <nallani@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 14:55:38 by nallani           #+#    #+#             */
-/*   Updated: 2022/12/09 01:55:28 by lmariott         ###   ########.fr       */
+/*   Updated: 2022/12/09 05:35:22 by lmariott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Clock.hpp"
+#include "Gameboy.hpp"
+
+bool Clock::cgbMode = false;
+int Clock::timaClock = 0;
+int Clock::divClock = 0;
 
 Clock::Clock()
 {
-	//TODO change that, after boot rom state
-	clock = 32916 / 2;
-	clockCount = clock / 114;
+	/* Init DIV at 0 */
+	mem[0xFF04] = 0;
+	/* Init TIMA at 0 */
+	mem[0xFF05] = 0;
+	clock = 0;
+
+	// TODO LMA whats this ?
+	// clock = 32916 / 2;
 }
 
 Clock::~Clock()
 {
-
 }
 
-int	Clock::operator+(int addValue)
-{
-	return clock + addValue;
-}
+#define M_DIV	mem[0xFF04]
+#define M_TIMA	mem[0xFF05]
+#define M_TMA	mem[0xFF06]
+#define M_TAC	mem[0xFF07]
 
 int&	Clock::operator+=(int addValue)
 {
+	int timaFreqDivider = 1024;
+
 	clock += addValue;
-	//TODO do it properly
 
-       unsigned int tmp = clock / 114;
+	/* Handle DIV */
+	divClock += addValue;
+	M_DIV += divClock / 256;
+	divClock %= 256;
 
-       if (tmp > clockCount)
-       {
-               clockCount += 1;
-       }
-	if (clock >= 17556) // 144 line + V-BLANK
-	{
-		// TODO Interrupt
-		clock = clock - 17556;
-		clockCount = 0;
+	/* Handle TIMA */
+	if ((M_TAC & (1 << 2)) != 0) {
+		/* Calculate TIMA frequency divider TODO CGB Mode is not handled */
+		if ((M_TAC & 0x3) == 1) {
+			timaFreqDivider = 16;
+		} else if ((M_TAC & 0x3) == 2) {
+			timaFreqDivider = 64;
+		} else if ((M_TAC & 0x3) == 3) {
+			timaFreqDivider = 256;
+		}
+		timaClock += addValue;
+		if ((int)M_TIMA + timaClock / timaFreqDivider > 0xFF) {
+			/* Request Interrupt */
+			mem[0xFF0F] |= (1 << 2);
+			/* Reset TIMA to TMA and add the rest */
+			M_TIMA = (int)M_TMA + (256 - ((int)mem[0xFF05] + timaClock / timaFreqDivider));
+			timaClock %= timaFreqDivider;
+		}
+		else {
+			M_TIMA += timaClock / timaFreqDivider;
+			timaClock %= timaFreqDivider;
+		}
 	}
-	return clock;
-}
-
-int	Clock::operator-(int subValue)
-{
-	return clock - subValue;
-}
-
-int&	Clock::operator-=(int subValue)
-{
-	clock -= subValue;
-	return clock;
-}
-
-int&	Clock::operator=(int value)
-{
-	clock = value;
+	if (clock >= 17556) {
+		// 144 line + V-BLANK
+		// We reset the counter here, we dont need to do it at this value but why not
+		clock = clock - 17556;
+	}
 	return clock;
 }
 
--- src/Clock.hpp
+++ src/Clock.hpp
@@ -6,7 +6,7 @@
 /*   By: nallani <nallani@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 14:55:36 by nallani           #+#    #+#             */
-/*   Updated: 2022/12/09 01:34:41 by lmariott         ###   ########.fr       */
+/*   Updated: 2022/12/09 04:35:51 by lmariott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,40 @@
 
 #include "functional"
 
-//TODO add frequencies etc..
+// Clock cycle freq is 4.194304 MHz for GB
+// and 8.4 MHz for CGB in double speed mode
+
+// FF04 : DIV (Divider Register)
+// FF04 is incremented at rate of 16384Hz ----> it's clock cycle of gb / 256 
+// and 32768Hz in CGB double speed mode. ------> same thing
+
+// FF05 : TIMA (Timer Counter)
+// Incremented according to clock freq from TAC register (FF07)
+// When the value overflow (> 0xFF, its a byte) it trigger an interrupt and it's reset to TMA (FF06)
+
+// FF06 : TMA
+// This data is loaded to TIMA when its overflow
+
+// FF07 : TAC (Timer Control)
+// Bit 2 : 1:Start 0:Stop
+// Bits 1-0 : 
+// 	00 -> 4096Hz -> clock cycle / (1024 * CGBMODE?2:1)
+// 	01 -> 262144Hz -> clock cycle / (16 * CGBMODE?2:1)
+// 	10 -> 65536Hz -> clock cycle / (64 * CGBMODE?2:1)
+// 	11 -> 16384Hz -> clock cycle / (256 * CGBMODE?2:1)
+
+
 class Clock {
 public:
 	Clock();
 	virtual ~Clock(void);
-	int operator+(int addValue);
-	int operator-(int subValue);
 	int& operator+=(int addValue);
-	int& operator-=(int subValue);
-	int& operator=(int value);
 	operator int();
 private:
-	int clock;
-	unsigned int clockCount;
+	int 			clock;
+	static bool		cgbMode;
+	static int		timaClock;
+	static int		divClock;
 };
 
 #endif
