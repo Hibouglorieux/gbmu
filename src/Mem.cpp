8c4bc96f5c964ca1d46a28acd209404fbc82da2e
Leon Mariotto <lmariott@student.42.fr>
2023-01-26 10:39:41 +0100

    close stream when invalid rom is loaded

--- src/Mem.cpp
+++ src/Mem.cpp
@@ -55,6 +55,7 @@ Mem* Mem::loadFromFile(const std::string& pathToRom)
 	{
 			std::cerr << "Error with rom at wrong format" << std::endl;
 			UserInterface::throwError("Error with rom at wrong format", false);
+			file.close();
 			return nullptr;
 			// throw("");
 	}
