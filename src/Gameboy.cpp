05cf44801b8fcedf3411c8c197dd2a97df965ca9
lucky <guvillat.villat.guillaume@gmail.com>
2023-01-29 21:23:07 +0100

    add check return of UserInterface::Create si /home/lucky not declarer = crash

--- src/Gameboy.cpp
+++ src/Gameboy.cpp
@@ -95,8 +95,9 @@ bool Gameboy::loadRom()
 
 bool Gameboy::launchUserInterface()
 {
-	UserInterface::create();
-	return UserInterface::loop();
+	if (UserInterface::create())
+	    return UserInterface::loop();
+    return false;
 }
 
 void Gameboy::clear()
